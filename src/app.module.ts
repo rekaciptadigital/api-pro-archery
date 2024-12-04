18c6a6261f1c48ee83854e13423bc6d14cf2127c
rekaciptadigital <135802862+rekaciptadigital@users.noreply.github.com>
2024-12-05 01:18:26 +0700

    import all module to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersModule } from "./modules/users/users.module";
+import { RolesModule } from "./modules/roles/roles.module";
+import { FeaturesModule } from "./modules/features/features.module";
+import { PermissionsModule } from "./modules/permissions/permissions.module";
+import { UserRolesModule } from "./modules/user-roles/user-roles.module";
 import { CommonModule } from "./common/common.module";
 import { TransformersModule } from "./common/transformers/transformers.module";
 import configuration from "./config/configuration";
@@ -32,6 +36,10 @@ import { validate } from "./config/env.validation";
       inject: [ConfigService],
     }),
     UsersModule,
+    RolesModule,
+    FeaturesModule,
+    PermissionsModule,
+    UserRolesModule,
     CommonModule,
     TransformersModule,
   ],
