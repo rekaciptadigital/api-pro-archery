6d90c31522d0ade9c6e7a3cbccc61e64699dd6ba
rekaciptadigital <135802862+rekaciptadigital@users.noreply.github.com>
2024-12-23 02:38:50 +0700

    Update src/modules/user-roles/domain/repositories/user-role.repository.ts [BoltSync]

--- src/modules/user-roles/domain/repositories/user-role.repository.ts
+++ src/modules/user-roles/domain/repositories/user-role.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { Repository, IsNull } from "typeorm";
 import { UserRole } from "../entities/user-role.entity";
 import { BaseRepository } from "../../../common/repositories/base.repository";
 
@@ -14,31 +14,51 @@ export class UserRoleRepository extends BaseRepository<UserRole> {
   }
 
   async findByUserAndRole(userId: number, roleId: number): Promise<UserRole | null> {
-    return this.userRoleRepository.findOne({
+    return this.repository.findOne({
       where: {
         user_id: userId,
         role_id: roleId,
+        deleted_at: IsNull()
       }
     });
   }
 
   async findByUser(userId: number): Promise<UserRole[]> {
-    return this.userRoleRepository.find({
-      where: { user_id: userId },
+    return this.repository.find({
+      where: { 
+        user_id: userId,
+        deleted_at: IsNull()
+      },
       relations: ["user", "role"],
     });
   }
 
   async findAllWithRelations(): Promise<UserRole[]> {
-    return this.userRoleRepository.find({
+    return this.repository.find({
+      where: { deleted_at: IsNull() },
       relations: ["user", "role"],
     });
   }
 
   async findOneWithRelations(id: number): Promise<UserRole | null> {
-    return this.userRoleRepository.findOne({
-      where: { id },
+    return this.repository.findOne({
+      where: { 
+        id,
+        deleted_at: IsNull()
+      },
       relations: ["user", "role"],
     });
+  }
+
+  async findWithDeleted(id: number): Promise<UserRole | null> {
+    return this.repository.findOne({
+      where: { id } as any,
+      withDeleted: true
+    });
+  }
+
+  async restore(id: number): Promise<UserRole | null> {
+    await this.repository.restore(id);
+    return this.findById(id);
   }
 }
