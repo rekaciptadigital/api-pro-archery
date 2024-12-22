16f8188c075a1457939c59df7ba6ebe15dc663fa
rekaciptadigital <135802862+rekaciptadigital@users.noreply.github.com>
2024-12-23 02:38:51 +0700

    Update src/modules/user-roles/application/services/user-role.service.ts [BoltSync]

--- src/modules/user-roles/application/services/user-role.service.ts
+++ src/modules/user-roles/application/services/user-role.service.ts
@@ -3,7 +3,7 @@ import { UserRoleRepository } from "../../domain/repositories/user-role.reposito
 import { CreateUserRoleDto, UpdateUserRoleDto, UpdateUserRoleStatusDto } from "../dtos/user-role.dto";
 import { UserRoleValidator } from "../../domain/validators/user-role.validator";
 import { ResponseTransformer } from "../../../../common/transformers/response.transformer";
-import { DomainException } from "../../../common/exceptions/domain.exception";
+import { DomainException } from "@/common/exceptions/domain.exception";
 import { HttpStatus } from "@nestjs/common";
 
 @Injectable()
@@ -15,7 +15,6 @@ export class UserRoleService {
   ) {}
 
   async create(createUserRoleDto: CreateUserRoleDto) {
-    // Validate user role assignment
     await this.userRoleValidator.validateForCreate(
       createUserRoleDto.user_id,
       createUserRoleDto.role_id
@@ -49,7 +48,6 @@ export class UserRoleService {
       throw new DomainException('User role not found', HttpStatus.NOT_FOUND);
     }
 
-    // Validate user role update
     await this.userRoleValidator.validateForUpdate(
       id,
       userRole.user_id,
@@ -77,6 +75,24 @@ export class UserRoleService {
     }
 
     await this.userRoleRepository.softDelete(id);
-    return this.responseTransformer.transformDelete('User role');
+    return this.responseTransformer.transform({ message: 'User role deleted successfully' });
+  }
+
+  async restore(id: number) {
+    const userRole = await this.userRoleRepository.findWithDeleted(id);
+    if (!userRole) {
+      throw new DomainException('User role not found', HttpStatus.NOT_FOUND);
+    }
+
+    if (!userRole.deleted_at) {
+      throw new DomainException('User role is not deleted', HttpStatus.BAD_REQUEST);
+    }
+
+    const restored = await this.userRoleRepository.restore(id);
+    if (!restored) {
+      throw new DomainException('Failed to restore user role', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    return this.responseTransformer.transform(restored);
   }
 }
